7bbf10927004cd8848c24039d8f842d4d0450a66
Szymon Cichy <dev@szycic.com>
2023-11-02 09:12:28 +0100

    PORT env

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const sqlite3 = require('sqlite3');
 const {open} = require('sqlite'); 
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 app.use(express.json());
